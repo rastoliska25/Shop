515975e15c895523842ae6c4a4a444fc9595fd5d
rastoliska25 <rasto.liska25@gmail.com>
2022-08-05 08:08:57 +0200

    kafka test message working

--- src/main/java/com/learn2code/Shop/db/service/Consumer.java
+++ src/main/java/com/learn2code/Shop/db/service/Consumer.java
@@ -1,18 +1,14 @@
 package com.learn2code.Shop.db.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Consumer {
-    public static final String topic = "mytopic";
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemp;
-
-    public void publishToTopic(String message) {
-        System.out.println("Publishing to topic "+topic);
-        this.kafkaTemp.send(topic, message);
+    @KafkaListener(topics="mytopic", groupId="mygroup")
+    public void consumeFromTopic(String message) {
+        System.out.println("Consummed message "+message);
     }
 }
