fe891217f5414b884f24b56c7f2452ddb30ffbf9
rastoliska25 <rasto.liska25@gmail.com>
2022-08-15 21:37:37 +0200

    APi definiton - YAML: http://localhost:8080/shop-docs.yaml

--- src/main/java/com/learn2code/shop/ShopApplication.java
+++ src/main/java/com/learn2code/shop/ShopApplication.java
@@ -1,11 +1,14 @@
 package com.learn2code.shop;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication()
+@OpenAPIDefinition(info = @Info(title = "Shop API", version = "1.0", description = "Shop microservice"))
 public class ShopApplication {
 
 	public static void main(String[] args) {
