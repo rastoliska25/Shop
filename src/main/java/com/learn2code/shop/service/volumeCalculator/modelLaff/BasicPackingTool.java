7eeae7ee1d6fcb699198c71c87f74e683c3a0556
rastoliska25 <rasto.liska25@gmail.com>
2022-08-18 14:14:35 +0200

    removed abstract class from laff calculation

--- src/main/java/com/learn2code/shop/service/volumeCalculator/modelLaff/BasicPackingTool.java
+++
@@ -1,7 +0,0 @@
-package com.learn2code.shop.service.volumeCalculator.modelLaff;
-
-import com.learn2code.shop.service.volumeCalculator.interfaces.PackingTool;
-
-public abstract class BasicPackingTool implements PackingTool {
-
-}
